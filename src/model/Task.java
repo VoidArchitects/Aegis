db6b4d4c28da3b8a077dda941fe39fae559b82a4
Krishna <kt345575@gmail.com>
2026-07-20 15:54:39 +0530

    feat: add TaskService with search and sorting support

---
+++ src/model/Task.java
@@ -0,0 +1,94 @@
+package model;
+
+import enums.Category;
+import enums.Priority;
+import java.time.LocalDate;
+
+public class Task{
+    private String title;
+    private String description;
+    private Category category;
+    private Priority priority;
+    private LocalDate deadline;
+    private boolean completed;
+
+    public Task(String title, String description, Category category, Priority priority, LocalDate deadline) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.priority = priority;
+        this.deadline = deadline;
+        this.completed = false;
+    }
+
+// =================================GETTERS=================================
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    // ================================SETTERS=================================
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public void markComplete() {
+        this.completed = true;
+    }
+    public void markIncomplete() {
+        this.completed = false;
+    }
+
+    // ================================TO STRING=================================
+    @Override
+    public String toString(){
+        return """
+                    =========================
+                              TASK
+                    =========================
+                    Title       : %s
+                    Description : %s
+                    Category    : %s
+                    Priority    : %s
+                    Deadline    : %s
+                    Completed   : %s
+                    ==========================
+            """.formatted(title, description, category, priority, deadline, completed? "Yes" : "No");
+    }
+}
---
+++ src/service/TaskService.java
@@ -0,0 +1,78 @@
+package service;
+import enums.Category;
+import enums.Priority;
+import java.util.ArrayList;
+import java.util.List;
+import model.Task;
+public class TaskService{
+    private final List<Task> tasks;
+    public TaskService(){
+        tasks = new ArrayList<>();
+    }
+    // ================================TASK MANAGEMENT METHODS=================================
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+    public void removeTask(Task task){
+        tasks.remove(task);
+    }
+    public List<Task> getAllTasks(){
+        return List.copyOf(tasks);
+    }
+    public void markTaskAsCompleted(Task task){
+        task.markComplete();
+    }
+    public void markTaskAsNotCompleted(Task task){
+        task.markIncomplete();
+    }
+    public int getTaskCount(){
+        return tasks.size();
+    }
+    public boolean hasTasks() {
+        return !tasks.isEmpty();
+    }
+    // =================================SEARCH METHODS=================================
+    public List<Task> findTasksByTitle(String title){
+        List<Task> result = new ArrayList<>();
+        for(Task task : tasks){
+            if(task.getTitle().equalsIgnoreCase(title)){
+                result.add(task);
+            }
+        }
+        return result;
+    }
+    public List<Task> findTasksByCategory(Category category){
+        List<Task> result = new ArrayList<>();
+        for(Task task : tasks){
+            if(task.getCategory().equals(category)){
+                result.add(task);
+            }
+        }
+        return result;
+    }
+    public List<Task> findTasksByPriority(Priority priority){
+        List<Task> result = new ArrayList<>();
+        for(Task task : tasks){
+            if(task.getPriority().equals(priority)){
+                result.add(task);
+            }
+        }
+        return result;
+    }
+    // ================================SORTING METHODS=================================
+    public List<Task> sortTasksByTitle(){
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+        sortedTasks.sort((a, b) -> a.getTitle().compareTo(b.getTitle()));
+        return List.copyOf(sortedTasks);
+    }
+    public List<Task> sortTasksByPriority(){
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+        sortedTasks.sort((a,b) -> a.getPriority().compareTo(b.getPriority()));
+        return List.copyOf(sortedTasks);
+    }
+    public List<Task> sortTasksByDeadline(){
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+        sortedTasks.sort((a,b) -> a.getDeadline().compareTo(b.getDeadline()));
+        return List.copyOf(sortedTasks);
+    }
+}
